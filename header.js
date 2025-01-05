4e85bd238e235dd107112e1dd8c9be2338e2a2bd
Bidias MELATAGUIA <70631719+melataguia@users.noreply.github.com>
2025-01-05 23:48:39 +0100

    Update header.js

--- header.js
+++ header.js
@@ -1,18 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     const themeToggle = document.getElementById('theme-toggle');
     const body = document.querySelector('body');
-     const navToggle = document.querySelector('.nav-toggle');
+    const navToggle = document.querySelector('.nav-toggle');
     const mainNav = document.querySelector('.main-nav');
+     let isMenuOpen = false;
+
     navToggle.addEventListener('click', () => {
-      mainNav.classList.toggle('active');
+        mainNav.classList.toggle('active');
+          isMenuOpen = !isMenuOpen;
     });
 
-    themeToggle.addEventListener('click', function() {
-        body.classList.toggle('light-theme');
-         if(body.classList.contains('light-theme')) {
-            themeToggle.textContent = "Thème sombre";
-        }else {
-            themeToggle.textContent = "Changer le thème";
+      themeToggle.addEventListener('click', function() {
+          body.classList.toggle('light-theme');
+            if(body.classList.contains('light-theme')) {
+                themeToggle.textContent = "Thème sombre";
+            }else {
+                themeToggle.textContent = "Changer le thème";
+            }
+     });
+
+         // Gestionnaire d'événements pour fermer le menu lorsqu'on clique à l'extérieur du menu
+    document.addEventListener('click', (event) => {
+        if (isMenuOpen && !mainNav.contains(event.target) && !navToggle.contains(event.target)) {
+            mainNav.classList.remove('active');
+            isMenuOpen = false;
         }
     });
 });
