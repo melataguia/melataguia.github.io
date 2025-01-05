c8ba926189b6626051737169bf7a37670b0579ac
Bidias MELATAGUIA <70631719+melataguia@users.noreply.github.com>
2025-01-05 23:31:02 +0100

    Update header.js

--- header.js
+++ header.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const themeToggle = document.getElementById('theme-toggle');
     const body = document.querySelector('body');
+     const navToggle = document.querySelector('.nav-toggle');
+    const mainNav = document.querySelector('.main-nav');
+      navToggle.addEventListener('click', () => {
+        mainNav.classList.toggle('active');
+        });
 
     themeToggle.addEventListener('click', function() {
         body.classList.toggle('light-theme');
