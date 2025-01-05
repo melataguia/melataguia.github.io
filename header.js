1da75864df73c8f93d4cdb4c5b34540c9118bdc3
Bidias MELATAGUIA <70631719+melataguia@users.noreply.github.com>
2025-01-05 21:46:00 +0100

    Create header.js

---
+++ header.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.querySelector('body');
+
+    themeToggle.addEventListener('click', function() {
+        body.classList.toggle('light-theme');
+         if(body.classList.contains('light-theme')) {
+            themeToggle.textContent = "Thème sombre";
+        }else {
+            themeToggle.textContent = "Changer le thème";
+        }
+    });
+});
