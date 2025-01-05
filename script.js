a5984006bbec0e61280ac6154534438e459f7159
Bidias MELATAGUIA <70631719+melataguia@users.noreply.github.com>
2025-01-06 00:32:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const body = document.getElementById('theme-body');
-      const link = document.createElement('link');
+    const body = document.querySelector('body');
+    const headerPlaceholder = document.getElementById('header-placeholder');
+     const link = document.createElement('link');
           link.rel = 'stylesheet';
           link.href = 'header.css';
          document.head.appendChild(link);
-
     fetch('header.html')
         .then(response => {
             if (!response.ok) {
@@ -13,8 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
             return response.text();
         })
         .then(data => {
-             document.getElementById('header-placeholder').innerHTML = data;
-          const themeToggle = document.getElementById('theme-toggle');
+             headerPlaceholder.innerHTML = data;
+           const themeToggle = document.getElementById('theme-toggle');
+              const navToggle = document.querySelector('.navbar-toggler');
+               let isMenuOpen = false;
+                    navToggle.addEventListener('click', () => {
+                       if (isMenuOpen){
+                         navToggle.setAttribute('aria-expanded', 'false');
+                            isMenuOpen = false;
+                        } else {
+                         navToggle.setAttribute('aria-expanded', 'true');
+                             isMenuOpen = true;
+                         }
+                    });
             themeToggle.addEventListener('click', function() {
                  body.classList.toggle('light-theme');
                 if(body.classList.contains('light-theme')) {
@@ -22,10 +33,10 @@ document.addEventListener('DOMContentLoaded', function() {
                  }else {
                     themeToggle.textContent = "Changer le thème";
                  }
-            });
+             });
          })
         .catch(error => {
             console.error('Failed to fetch header:', error);
-            document.getElementById('header-placeholder').innerHTML = '<p>Failed to load header.</p>';
+             headerPlaceholder.innerHTML = '<p>Failed to load header.</p>';
         });
 });
